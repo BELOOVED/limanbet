b3b1d99075ec578c99425b2e81db323b78de12a2
Illumincer <illumincer@gmail.com>
2024-01-28 14:07:07 +0300

    aaa

--- view/modules/footer.php
+++ view/modules/footer.php
@@ -2680,7 +2680,7 @@
             </div>
          </div>
       </bet-master>
-      <script src="https://cdn.tutorialjinni.com/OwlCarousel2/2.3.4/owl.carousel.js"></script>  
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
       <script>
       function openmenu(element) {
        var tabId = $(element).attr('id');
