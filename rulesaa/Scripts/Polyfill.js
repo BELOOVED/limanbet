23b38dadedd5cc642d878c55f109275c5e1bf46b
N1C3B00T <77572556+N1C3B00T@users.noreply.github.com>
2024-02-02 13:27:09 +0300

    ef

--- rulesaa/Scripts/Polyfill.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-/***********************************************  String  ***********************************************/
-if (!String.prototype.replaceAll) {
-	String.prototype.replaceAll = function (str, newStr) {
-		return this.split(str).join(newStr);
-	};
-}
