70d99e5d6194feec8d25655ad109deac386e67cc
N1C3B00T <77572556+N1C3B00T@users.noreply.github.com>
2024-02-15 12:14:59 +0300

    asd

--- request.php
+++ request.php
@@ -4,9 +4,9 @@ include "inc/functions.php";
 include "inc/anti.php";
 include "inc/check.php";
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 
 $q = $_GET["q"];
